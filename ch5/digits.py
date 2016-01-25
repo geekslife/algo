68cef58f909e6124c35ce7f78f549737d5eb5c2e
Haeyoung Kim <geekslife@gmail.com>
2016-01-25 20:23:18 +0900

    completed digits.py

--- ch5/digits.py
+++ ch5/digits.py
@@ -1,24 +1,30 @@
 def interesting(base):
+    print 'interesting(',base,')'
     digits = []
     # for all digits choose b,
     #   set 3 digit number with mod b == 0
     #   check if sum mod b == 0
     #   if pass, add to digits
     for b in range(2,base):
-        num = base**2+base
-        while num%b!=0:
-            num += 1
-        d3 = num/base**2
-        res = num-d3*base**2
-        d2 = res/base
-        res = res-d2*base
-        d1 = res
-        sum_digits = d3+d2+d1
-        print b,':',num,'=>',d3,d2,d1,',sum=',sum_digits
-        if sum_digits%b == 0:
-            print "!",b
-            digits.append(b)
+        #print '... ',b,'/',base
+        valid = True
+        for d1 in range(0,base):
+            for d2 in range(0,base):
+                for d3 in range(0,base):
+                    n = d1*base**2 + d2*base + d3
+                    if n % b == 0 and (d1+d2+d3)%b != 0:
+                        valid=False
+                        break
+                if not valid: break
+            if not valid: break
+        if valid :
+                #print "!",b
+                digits.append(b)
     return digits
 
 if __name__ == '__main__':
     assert [ 3, 9 ] == interesting(10)
+    assert [ 2 ] == interesting(3)
+    assert [ 2,4,8 ] == interesting(9)
+    assert [5,25] == interesting(26)
+    assert [29] == interesting(30)
